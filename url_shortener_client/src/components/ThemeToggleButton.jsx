fe202f1869f950b7253ba8953dc0844a159fd7da
Abhishek <abhikr.ak.2001@gmail.com>
2024-12-01 13:27:23 +0530

    theme togggle button

--- url_shortener_client/src/components/ThemeToggleButton.jsx
+++ url_shortener_client/src/components/ThemeToggleButton.jsx
@@ -1,18 +1,20 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "../ThemeContext";
-import { WiDaySunny } from "react-icons/wi";
-import { MdNightlight } from "react-icons/md";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import Fab from "@mui/material/Fab";
 
 const ThemeToggleButton = () => {
   const { theme, toggleTheme } = useContext(ThemeContext);
 
   return (
-    <button
+    <Fab
+      size="medium"
       onClick={toggleTheme}
-      className="rounded-full bg-blue-500 px-4 py-2 text-white transition-all duration-500 dark:bg-yellow-500"
+      className="transition-all duration-500"
     >
-      {theme === "light" ? <MdNightlight /> : <WiDaySunny />}
-    </button>
+      {theme === "light" ? <DarkModeIcon /> : <LightModeIcon />}
+    </Fab>
   );
 };
 
