e6d319219086dda785d882ca798c6868d7d26285
Abhishek <abhikr.ak.2001@gmail.com>
2024-12-01 13:27:32 +0530

    input field

--- url_shortener_client/src/components/InputField.jsx
+++ url_shortener_client/src/components/InputField.jsx
@@ -1,11 +1,5 @@
 import React, { useState } from "react";
-import {
-  FormControl,
-  InputLabel,
-  OutlinedInput,
-  InputAdornment,
-  Button,
-} from "@mui/material";
+import { Box, Button, TextField } from "@mui/material";
 
 const InputField = () => {
   const [inputValue, setInputValue] = useState("");
@@ -14,7 +8,8 @@ const InputField = () => {
   // URL validation regex
   const urlRegex = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/;
 
-  const handleSubmit = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevents form from reloading the page
     if (!urlRegex.test(inputValue)) {
       setError("Please enter a valid website URL.");
     } else {
@@ -25,42 +20,49 @@ const InputField = () => {
   };
 
   return (
-    <div className="flex flex-col items-center space-y-2 px-4">
-      <div className="flex w-96 max-w-4xl items-center space-x-4">
-        <FormControl
-          fullWidth
-          className="w-96"
-          sx={{
-            flexGrow: 1,
-            maxWidth: { xs: "100%", sm: "75%", lg: "600px" },
-          }}
-        >
-          <InputLabel
-            htmlFor="outlined-adornment-url"
-            className="dark:text-white"
-          >
-            Website URL
-          </InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-url"
+    <div className="first-line flex flex-col items-center space-y-2 px-4">
+      <form
+        className="flex w-96 max-w-4xl items-center space-x-4"
+        onSubmit={handleSubmit}
+      >
+        <Box sx={{ flexGrow: 1 }}>
+          <TextField
+            id="outlined-basic"
+            label="Enter URL"
+            variant="filled"
             value={inputValue}
             onChange={(e) => setInputValue(e.target.value)}
-            startAdornment={
-              <InputAdornment position="start">🌐</InputAdornment>
-            }
-            label="Website URL"
-            className="dark:text-white"
+            fullWidth
+            error={!!error} // Highlights the field in red if there's an error
+            sx={{
+              "& .MuiFilledInput-root": {
+                bgcolor: "white", // Background color of the input
+                color: "black", // Text color of the input
+                "&:hover": {
+                  bgcolor: "white", // Ensure hover doesn't change background
+                },
+                "&.Mui-focused": {
+                  bgcolor: "white", // Ensure focus doesn't change background
+                },
+              },
+              "& .MuiInputLabel-root": {
+                color: "black", // Label text color
+              },
+              "& .MuiInputLabel-root.Mui-focused": {
+                color: "black", // Focused label text color
+              },
+            }}
           />
-        </FormControl>
+        </Box>
         <Button
+          type="submit"
           variant="contained"
           className="!bg-green-700 text-white hover:!bg-green-800"
-          onClick={handleSubmit}
           size="large"
         >
-          Submit
+          Convert
         </Button>
-      </div>
+      </form>
       <div className="h-5 w-full max-w-4xl self-start">
         {/* Reserve space for the error message */}
         {error && (
