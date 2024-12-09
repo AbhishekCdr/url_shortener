e1967cf043dc3e96de49240f85ed5b3ab38821c9
Abhishek <abhikr.ak.2001@gmail.com>
2024-12-09 23:31:50 +0530

    axios

--- url_shortener_client/src/pages/Home.jsx
+++ url_shortener_client/src/pages/Home.jsx
@@ -7,6 +7,7 @@ import InputField from "../components/InputField.jsx";
 import TransitionModal from "../Modal/TransitionModal.jsx";
 import SignUp from "./SignUp.jsx";
 import SignIn from "./SignIn.jsx";
+import axios from "axios";
 
 const Home = () => {
   const [open, setOpen] = useState(false);
@@ -15,24 +16,61 @@ const Home = () => {
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   const SignUpOpen = () => setLogin((old) => !old);
+  let user = null;
+  user = localStorage.getItem("username");
+
+  const handleSignout = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:3000/v1/api/auth/signout",
+        { withCredentials: true },
+      );
+      console.log(response.data);
+
+      alert("You have been logged out successfully!");
+      localStorage.removeItem("username");
+      window.location.reload();
+    } catch (error) {
+      console.error("Error during signout:", error);
+    }
+  };
 
   return (
     <div className="bg-dark-svg flex w-screen flex-grow flex-col items-center gap-12 p-5">
       <div className="flex items-center gap-10 self-end">
-        <div>
-          <Button
-            size="medium"
-            variant="contained"
-            className="!bg-blue-600 hover:!bg-blue-700"
-            onClick={handleOpen}
-          >
-            LogIn
-          </Button>
+        <div className="dark:text-white">
+          <div className="flex items-center gap-3">
+            {user ? (
+              <span>
+                Welcome <span className="text-lg font-semibold">{user}</span>
+              </span>
+            ) : (
+              <Button
+                size="medium"
+                variant="contained"
+                className="!bg-blue-600 hover:!bg-blue-700"
+                onClick={handleOpen}
+              >
+                LogIn
+              </Button>
+            )}
+            {user && (
+              <Button
+                size="medium"
+                variant="contained"
+                className="!bg-red-600 hover:!bg-red-700"
+                onClick={handleSignout}
+              >
+                LogOut
+              </Button>
+            )}
+          </div>
+
           <TransitionModal open={open} handleClose={handleClose}>
             {login ? (
-              <SignIn SignUpOpen={SignUpOpen} />
+              <SignIn SignUpOpen={SignUpOpen} handleClose={handleClose} />
             ) : (
-              <SignUp SignUpOpen={SignUpOpen} />
+              <SignUp SignUpOpen={SignUpOpen} handleClose={handleClose} />
             )}
           </TransitionModal>
         </div>
