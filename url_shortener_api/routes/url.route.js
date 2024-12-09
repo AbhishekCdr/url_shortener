dad56eef45e732463993997d0d2ecaf0b61d2d57
Abhishek <abhikr.ak.2001@gmail.com>
2024-12-09 23:31:31 +0530

    verify user

--- url_shortener_api/routes/url.route.js
+++ url_shortener_api/routes/url.route.js
@@ -3,12 +3,16 @@ import {
   createShortUrl,
   getUrl,
   deleteUrl,
+  isActive,
 } from "../controllers/url.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/create-short-url", createShortUrl);
+router.get("/get-all-short-urls-user/:userName", verifyToken, getUrl);
 router.get("/get-all-short-urls/:userName", getUrl);
 router.delete("/delete/:_id", deleteUrl);
+router.patch("/isActive/:_id", isActive);
 
 export default router;
